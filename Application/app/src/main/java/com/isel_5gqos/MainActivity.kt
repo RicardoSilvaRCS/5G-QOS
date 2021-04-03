221a80cd1a7eb0a9f92dabed8b8f1d85f32fbf21
ricardo silva <silvacricardo@hotmail.com>
2021-04-03 16:56:53 +0100

    Starting app

---
+++ Application/app/src/main/java/com/isel_5gqos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.isel_5gqos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
