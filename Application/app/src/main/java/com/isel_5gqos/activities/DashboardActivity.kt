718db72f08c9138914adeb7c5fd7b3766868b830
ricardo silva <silvacricardo@hotmail.com>
2021-05-06 21:27:41 +0100

    trash

--- Application/app/src/main/java/com/isel_5gqos/activities/DashboardActivity.kt
+++ Application/app/src/main/java/com/isel_5gqos/activities/DashboardActivity.kt
@@ -1,24 +1,31 @@
 package com.isel_5gqos.activities
 
-import android.R
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Legend.LegendForm
 import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition
 import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
 import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.Utils
 import com.isel_5gqos.QosApp
+import com.isel_5gqos.R
 import com.isel_5gqos.common.DEFAULT_SESSION_ID
 import com.isel_5gqos.common.TAG
 import com.isel_5gqos.common.db.asyncTask
@@ -189,9 +196,76 @@ class DashboardActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, 
         tvX.text = seekBarX.progress.toString()
         tvY.text = seekBarY.progress.toString()
 
-        //setData(seekBarX.progress, seekBarY.progress);
+        setData(seekBarX.progress, seekBarY.progress)
 
         chart.invalidate();
+    }
+
+    private fun setData( throughPuts : List<ThroughPutDto>) {
+
+        val rxValues = mutableListOf<Entry>()
+        val txValues = mutableListOf<Entry>()
+
+       throughPuts.forEachIndexed { index, throughPutDto ->
+           rxValues.add(Entry(index.toFloat(),throughPutDto.rxResult.toFloat()))
+           txValues.add(Entry(index.toFloat(),throughPutDto.txResult.toFloat()))
+       }
+
+        val set1: LineDataSet
+        if (chart.data != null &&
+            chart.data.dataSetCount > 0
+        ) {
+            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = rxValues
+            set1.notifyDataSetChanged()
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(rxValues, "Rx")
+            set1.setDrawIcons(false)
+
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f)
+
+            // black lines and points
+            set1.color = Color.BLACK
+            set1.setCircleColor(Color.BLACK)
+
+            // line thickness and point size
+            set1.lineWidth = 1f
+            set1.circleRadius = 3f
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false)
+
+            // customize legend entry
+            set1.formLineWidth = 1f
+            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            set1.formSize = 15f
+
+            // text size of values
+            set1.valueTextSize = 9f
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f)
+
+            // set the filled area
+            set1.setDrawFilled(true)
+            set1.fillFormatter = IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
+
+
+            set1.fillColor = Color.BLACK
+
+            val dataSets: ArrayList<ILineDataSet> = ArrayList()
+            dataSets.add(set1) // add the data sets
+
+            // create a data object with the data sets
+            val data = LineData(dataSets)
+
+            // set data
+            chart.data = data
+        }
     }
 
     override fun onStartTrackingTouch(seekBar: SeekBar?) {}
