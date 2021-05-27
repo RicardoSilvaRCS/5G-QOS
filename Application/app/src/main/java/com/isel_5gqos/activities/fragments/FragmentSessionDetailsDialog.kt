3a6e7d6a3289b5e574e2798ab29dfe2cc24ffbb9
Afonso Nobre <anobre@six-factor.com>
2021-05-28 00:50:37 +0100

    Did some differences

--- Application/app/src/main/java/com/isel_5gqos/activities/fragments/FragmentSessionDetailsDialog.kt
+++ Application/app/src/main/java/com/isel_5gqos/activities/fragments/FragmentSessionDetailsDialog.kt
@@ -43,67 +43,19 @@ class FragmentSessionDetailsDialog:DialogFragment() {
                 .Builder(requireContext())
                 .create()
 
-            /* .setTitle("Bro, you sure?")
-             .setMessage("Sure sure?")
-             .setPositiveButton("I'm sure") {dialogInterface,_ ->
-                 dialogInterface.dismiss()
-             }
-             .setNegativeButton("Fuck that"){dialogInterface,_->
-                 dialogInterface.dismiss()
-             }
-             .setNeutralButton("Not sure"){disalogInterface,_-> AndroidUtils.makeBurnedToast(requireContext(),"Pussy")}
-             .show()*/
-            val animSet = AnimationSet(true)
-            animSet.interpolator = DecelerateInterpolator()
-            animSet.fillAfter = true
-            animSet.isFillEnabled = true
-
             val inflater = LayoutInflater.from(requireContext())
             val inflatedView = inflater.inflate(resources.getLayout(R.layout.delete_session_alert_dialog),null)
             val confirmButton = inflatedView.findViewById<Button>(R.id.session_delete_confirm_button)
-            confirmButton.setOnClickListener {
-                val power = (-1.0).pow(nrOfClicks.toDouble()).toFloat()
-                Log.v("powerValue",power.toString())
-                Log.v("powerValue",nrOfClicks.toString())
-                val animRotate = RotateAnimation(
-                    0.0f, (-18000.0f * nrOfClicks++ * power) ,
-                    RotateAnimation.RELATIVE_TO_SELF, 0.5f,
-                    RotateAnimation.RELATIVE_TO_SELF, 0.5f
-                )
-
-                animRotate.duration = 600000
-                animRotate.fillAfter = true
-                animSet.addAnimation(animRotate)
-
-                inflatedView.animation = animRotate
-                inflatedView.startAnimation(animSet)
-            }
+            confirmButton.setOnClickListener {dialog.dismiss()}
             val deleteButton = inflatedView.findViewById<Button>(R.id.session_delete_cancel_button)
-            deleteButton.setOnClickListener { }
+            deleteButton.setOnClickListener { dialog.dismiss() }
 
-
-            val animRotate = RotateAnimation(
-                0.0f, -18000.0f,
-                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
-                RotateAnimation.RELATIVE_TO_SELF, 0.5f
-            )
-
-            animRotate.duration = 600000
-            animRotate.fillAfter = true
-            animSet.addAnimation(animRotate)
 
             dialog.setView(inflatedView)
             dialog.setCanceledOnTouchOutside(false)
             dialog.window!!.setBackgroundDrawableResource(R.drawable.white_background_round_20)
             dialog.show()
 
-            inflatedView.startAnimation(animSet)
         }
-//        val metrics = resources.displayMetrics
-//        val width = metrics.widthPixels
-//        val height = metrics.heightPixels
-//
-//        dialog.window!!.setLayout((width*0.75).toInt(),(height * 0.75).toInt())
-//        text.text = "O Ricky boy é um gay"
     }
 }
